b04e50673a7358583c347dcf3362f96068eaea4a
rogerchen12 <roger.h.chen12@gmail.com>
2021-12-05 14:36:33 -0600

    circle

--- core/src/com/realm/Realm.java
+++ core/src/com/realm/Realm.java
@@ -4,28 +4,28 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.GL20;
 
 public class Realm extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+	ShapeRenderer shape;
+	int x = 50;
+	int y = 50;
+	int xSpeed = 5;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		shape = new ShapeRenderer();
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		x+=5;
+		shape.begin(ShapeRenderer.ShapeType.Filled);
+		shape.circle(x, y, 50);
+		shape.end();
 	}
 }
